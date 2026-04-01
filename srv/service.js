af1bfa2d2a1b901b983b3d67be0289cd68297ee3
EMIRHAN CELIK <emirhan.celik@clarity.cx>
2026-04-01 12:17:08 +0000

    test: CI/CD pipeline verification

--- srv/service.js
+++ srv/service.js
@@ -4,7 +4,7 @@ module.exports = cds.service.impl(async function () {
     const bpSrv = await cds.connect.to('API_BUSINESS_PARTNER')
 
     const { A_BusinessPartner } = this.entities
-    // test ci/cd
+    
     this.on('READ', 'A_BusinessPartner', async (req) => {
         return bpSrv.run(req.query)
     })
